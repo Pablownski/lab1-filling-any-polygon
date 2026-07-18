d86af00a71bd2b04167bd592fad734a346215c77
Pablownski <sal24374@uvg.edu.gt>
2026-07-18 13:11:03 -0600

    Add polygon 2 renderer: blue fill with white Bresenham border

--- polygon-filling/src/main.rs
+++ polygon-filling/src/main.rs
@@ -1,6 +1,6 @@
 mod fill;
 mod line;
-mod poligono1;
+mod poligono2;
 
 use image::{Rgb, RgbImage};
 
@@ -10,7 +10,7 @@ const HEIGHT: u32 = 500;
 fn main() {
     let mut img = RgbImage::from_pixel(WIDTH, HEIGHT, Rgb([0, 0, 0]));
 
-    poligono1::draw(&mut img);
+    poligono2::draw(&mut img);
 
     img.save("out.bmp").expect("failed to save image");
     println!("Saved out.bmp ({}x{})", WIDTH, HEIGHT);
