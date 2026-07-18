db4d58e1b8cd2871f7ae46c250d485090bac13bb
Pablownski <sal24374@uvg.edu.gt>
2026-07-18 13:21:45 -0600

    Also export out.png alongside out.bmp

--- polygon-filling/src/main.rs
+++ polygon-filling/src/main.rs
@@ -12,6 +12,7 @@ fn main() {
 
     poligono4::draw(&mut img);
 
+    img.save("out.png").expect("failed to save image");
     img.save("out.bmp").expect("failed to save image");
-    println!("Saved out.bmp ({}x{})", WIDTH, HEIGHT);
+    println!("Saved out.png and out.bmp ({}x{})", WIDTH, HEIGHT);
 }
