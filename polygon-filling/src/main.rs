dd41abc6d2b98411d007627d87ba059ad0722404
Pablownski <sal24374@uvg.edu.gt>
2026-07-18 13:12:32 -0600

    Add polygon 3 renderer: red fill with white Bresenham border

--- polygon-filling/src/main.rs
+++ polygon-filling/src/main.rs
@@ -1,7 +1,6 @@
 mod fill;
 mod line;
-mod poligono1;
-mod poligono2;
+mod poligono3;
 
 use image::{Rgb, RgbImage};
 
@@ -11,8 +10,7 @@ const HEIGHT: u32 = 500;
 fn main() {
     let mut img = RgbImage::from_pixel(WIDTH, HEIGHT, Rgb([0, 0, 0]));
 
-    poligono1::draw(&mut img);
-    poligono2::draw(&mut img);
+    poligono3::draw(&mut img);
 
     img.save("out.bmp").expect("failed to save image");
     println!("Saved out.bmp ({}x{})", WIDTH, HEIGHT);
